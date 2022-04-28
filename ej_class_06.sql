4432c50a47ae321847cb5c7c5541f6673680723c
mateolanz <53521898+mateolanz@users.noreply.github.com>
2022-04-27 21:43:16 -0300

    Add files via upload

---
+++ ej_class_06.sql
@@ -0,0 +1,35 @@
+SELECT first_name, last_name
+FROM actor a1
+WHERE EXISTS(SELECT * FROM actor a2 WHERE a1.last_name = a2.last_name AND a1.actor_id <> a2.actor_id) ORDER BY last_name;
+
+SELECT first_name, last_name
+FROM actor a
+WHERE NOT EXISTS(SELECT first_name, last_name FROM film_actor fa WHERE a.actor_id = fa.actor_id);
+
+SELECT first_name, last_name
+FROM customer c
+WHERE 1=(SELECT COUNT(*) FROM rental r WHERE c.customer_id = r.customer_id);
+
+SELECT first_name, last_name
+FROM customer c
+WHERE 1<(SELECT COUNT(*) FROM rental r WHERE c.customer_id = r.customer_id);
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id
+IN(SELECT actor_id FROM film_actor WHERE film_id IN(SELECT film_id FROM film WHERE title LIKE 'BETRAYED REAR' OR title LIKE 'CATCH AMISTAD'));
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id
+IN(SELECT actor_id FROM film_actor WHERE film_id IN(SELECT film_id FROM film WHERE title LIKE 'BETRAYED REAR' AND title NOT LIKE 'CATCH AMISTAD'));
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id
+IN(SELECT actor_id FROM film_actor WHERE film_id IN(SELECT film_id FROM film WHERE title LIKE 'BETRAYED REAR' AND title LIKE 'CATCH AMISTAD'));
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id
+IN(SELECT actor_id FROM film_actor WHERE film_id NOT IN(SELECT film_id FROM film WHERE title LIKE 'BETRAYED REAR' OR title LIKE 'CATCH AMISTAD'));
