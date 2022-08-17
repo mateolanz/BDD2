3fabfb0da1b0c6aeb48834c5648deca4593ed3fd
mateolanz <53521898+mateolanz@users.noreply.github.com>
2022-08-17 15:02:20 -0300

    Class 17 wacho🐧

--- ej_class_17.sql
+++ ej_class_17.sql
@@ -1,1 +1,33 @@
+/*1*/
+SELECT *
+FROM address a
+WHERE postal_code BETWEEN 1000 and 50000;
+SELECT *
+FROM address a 
+	INNER JOIN city c USING(city_id)
+	INNER JOIN country c2 USING(country_id)
+WHERE postal_code like '%13%';
+CREATE INDEX postalCode ON address(postal_code);
+show index from address;
+DROP INDEX postalCode ON address;
 
+/*2*/
+SHOW INDEX FROM actor;
+SELECT a.first_name 
+FROM actor a
+ORDER BY a.first_name DESC;/*4ms */
+SELECT a2.last_name
+FROM actor a2
+ORDER BY a2.last_name DESC;/*2ms */
+
+
+/*3*/
+
+SELECT f.description
+FROM film f
+WHERE f.description LIKE '%Documentary%';
+SHOW INDEX FROM film;
+CREATE FULLTEXT INDEX desc_index ON film(description);
+SELECT f.description 
+FROM film f 
+WHERE MATCH(f.description) AGAINST('%Documentary%');
