f2921e4e2da0d224875d93f391527980f3710063
mateolanz <53521898+mateolanz@users.noreply.github.com>
2022-08-17 11:47:27 -0300

    done till ex4

--- ej_class_15.sql
+++ ej_class_15.sql
@@ -11,11 +11,44 @@ INNER JOIN address a USING(address_id)
 INNER JOIN city ci USING(city_id)
 INNER JOIN country co USING(country_id);
 
-  
+
+ 
 /*2*/
+CREATE OR REPLACE VIEW film_details AS
+SELECT f.film_id, f.title, f.description, f.rental_rate, f.`length`, f.rating, GROUP_CONCAT(CONCAT_WS(" ", a.first_name, a.last_name) SEPARATOR ",") AS actors
+
 FROM film f 
 INNER JOIN film_category fc USING(film_id)
 INNER JOIN category c USING(category_id)
 INNER JOIN film_actor fa USING(film_id)
 INNER JOIN actor a USING(actor_id)
 GROUP BY film_id;
+
+
+
+/*3*/
+CREATE OR REPLACE VIEW sales_by_film_category AS
+SELECT DISTINCT c.name, SUM(p.amount) as total_rental
+
+FROM category c
+INNER JOIN film_category fc USING(category_id)
+INNER JOIN film f USING(film_id)
+INNER JOIN inventory i USING(film_id)
+INNER JOIN rental r USING(inventory_id)
+INNER JOIN payment p USING(rental_id)
+GROUP BY c.name;
+
+
+
+/*4*/
+CREATE OR REPLACE VIEW actor_information AS
+SELECT a.actor_id, CONCAT(a.first_name, " ", a.last_name), (SELECT COUNT(f.film_id)
+
+FROM film f
+INNER JOIN film_actor fa USING(film_id)
+INNER JOIN actor a2 USING(actor_id)
+WHERE a2.actor_id = a.actor_id) AS 'amount of films he/she acted on'
+FROM actor a;
+
+
+
